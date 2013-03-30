76cff81da2e4925f580e5e3bc19357019a6f969d
Roger Lipscombe <roger@differentpla.net>
2013-03-30 20:14:31 +0000

    Refactoring.

--- Spikes/ConvertMusic/Program.cs
+++ Spikes/ConvertMusic/Program.cs
@@ -19,19 +19,6 @@ namespace ConvertMusic
             string sourceFileName = args[0];
             string destinationFileName = args[1];
 
-            // Is it worth doing content negotiation? That is:
-            // I have WAV, I want FLAC. FLAC takes WAV => OK.
-            // I have FLAC, I want MP3. MP3 takes WAV => how to get WAV from FLAC? => OK.
-
-            // Register the encoder/decoder
-            string decoderPath = ConfigurationManager.AppSettings["DecoderPath"];
-            string decoderArguments = ConfigurationManager.AppSettings["DecoderArguments"];
-
-            string encoderPath = ConfigurationManager.AppSettings["EncoderPath"];
-            string encoderArguments = ConfigurationManager.AppSettings["EncoderArguments"];
-
-            // TODO: Progress.
-
             var cancellationTokenSource = new CancellationTokenSource();
             Console.CancelKeyPress += (sender, e) =>
                 {
@@ -40,18 +27,31 @@ namespace ConvertMusic
                     e.Cancel = true;
                 };
 
+            Transcoder.Convert(sourceFileName, destinationFileName, cancellationTokenSource.Token);
+        }
+    }
+
+    internal static class Transcoder
+    {
+        public static void Convert(string sourceFileName, string destinationFileName, CancellationToken cancellationToken)
+        {
+            // TODO: Have a registry for these, and some content negotiation.
+            string decoderPath = ConfigurationManager.AppSettings["DecoderPath"];
+            string decoderArguments = ConfigurationManager.AppSettings["DecoderArguments"];
+
+            string encoderPath = ConfigurationManager.AppSettings["EncoderPath"];
+            string encoderArguments = ConfigurationManager.AppSettings["EncoderArguments"];
+
             // Wire a graph together.
             var source = File.OpenRead(sourceFileName);
             var decoder = new CodecProcess(decoderPath, decoderArguments);
             var encoder = new CodecProcess(encoderPath, encoderArguments);
             var destination = File.Create(destinationFileName);
 
-            var cancellationToken = cancellationTokenSource.Token;
-
             const int bufferSize = 16384;
 
             decoder.ErrorDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
-            var decoderTask = decoder.Start(cancellationTokenSource.Token);
+            var decoderTask = decoder.Start(cancellationToken);
 
             // Note that the process has to be started before you can get the stream,
             // otherwise you get InvalidOperationException containing "StandardIn has not been redirected.".
@@ -60,7 +60,7 @@ namespace ConvertMusic
                       .ContinueWith(t => decoder.InputStream.Close());
 
             encoder.ErrorDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
-            var encoderTask = encoder.Start(cancellationTokenSource.Token);
+            var encoderTask = encoder.Start(cancellationToken);
 
             var decoderToEncoderTask =
                 decoder.OutputStream
@@ -74,13 +74,13 @@ namespace ConvertMusic
 
             Task.WhenAll(sourceToDecoderTask, decoderTask, decoderToEncoderTask, encoderTask, encoderToDestination)
                 .ContinueWith(t =>
+                {
+                    if (t.IsCanceled || t.IsFaulted)
                     {
-                        if (t.IsCanceled || t.IsFaulted)
-                        {
-                            Console.WriteLine("Deleting '{0}'.", destinationFileName);
-                            File.Delete(destinationFileName);
-                        }
-                    })
+                        Console.WriteLine("Deleting '{0}'.", destinationFileName);
+                        File.Delete(destinationFileName);
+                    }
+                })
                 .Wait();
         }
     }
